26bb9c0011c2e9d8e79d39539b14ad38791c6eb1
Fabric and Fabric Bolt Service User <fabric@it-util-1.build.internal>
2015-01-21 14:23:05 -0800

    Modified to take hosts from Fabric-Bolt, and parameters from bolt

--- fabfile.py
+++ fabfile.py
@@ -23,45 +23,45 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
-#Globals.  These could also be Fabric-Bolt parameters
-env.key_filename = '/opt/fabric/.ssh/id_rsa'
-env.reject_unknown_hosts = False
-env.disable_known_hosts = True
+# Globals.  These could also be Fabric-Bolt parameters
+# env.key_filename = '/opt/fabric/.ssh/id_rsa'
+# env.reject_unknown_hosts = False
+# env.disable_known_hosts = True
 env.path = '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 # env.shell = "/bin/bash -l -c"
 # env.use_shell = True
 # env.always_use_pty = True
 
-config_file = '/opt/fabric/.f5config'
-config = {}
 f5man = None
-with open("%s" % (config_file), 'r') as inFile:
-    config = json.load(inFile)
-    try:
-        f5_config = config['F5_config']
-        f5man = F5Manager(**f5_config)
-    except Exception:
-        traceback.print_exc()
 
 # Global wrapper for F5 control
-# def bounceF5(members, port, jsonpools):
 def bounceF5():
     def closuref(func):
         def innerclosuref(*args, **kwargs):
-            if(f5man):  # F5 Manager instantiated
-                pools = json.loads(env.f5pools)
-                for host in env.hosts:
-                    member = {'address': socket.gethostbyname(host), 'port': env.memberport}
+            global f5man
+            try:
+                if not(f5man):  # F5 Manager instantiated
+                    f5_config = {
+                        "server": env.f5ip,
+                        "port": int(env.f5port),
+                        "user": env.f5user,
+                        "passw": env.f5pass
+                    }
+                    f5man = F5Manager(**f5_config)
+                if(f5man):  # F5 Manager instantiated
+                    pools = json.loads(env.f5pools)
+                    member = {'address': socket.gethostbyname(env.host), 'port': env.memberport}
                     f5man.disableMember(member, pools)
                     func(*args, **kwargs)
                     f5man.enableMember(member, pools)
-            else:
-              logging.critical(red('F5 Manager failed to instantiate.'))
+                else:
+                  logging.critical(red('F5 Manager failed to instantiate.'))
+            except Exception:
+                traceback.print_exc()
         return wraps(func)(innerclosuref)
     return closuref
 
 @task
-# @bounceF5(env.hosts, env.memberport, env.f5pools)
 @bounceF5()
 @parallel
 def Restart():
